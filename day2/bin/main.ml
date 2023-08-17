4655e481d3c5738128e0ccf6adab2ec0c6ac1159
Petri-Johan Last <petri.last@sourcegraph.com>
2023-08-17 06:19:58 +0200

    Update day2 to use proper types

--- day2/bin/main.ml
+++ day2/bin/main.ml
@@ -2,46 +2,74 @@ let read_whole_file file =
   In_channel.open_text file |> In_channel.input_all |> String.split_on_char '\n'
 
 (* Part 1 *)
-let calc_round opp me =
-  match opp with
-  | "A" -> ( match me with "X" -> 4 | "Y" -> 8 | "Z" -> 3 | _ -> 0)
-  | "B" -> ( match me with "X" -> 1 | "Y" -> 5 | "Z" -> 9 | _ -> 0)
-  | "C" -> ( match me with "X" -> 7 | "Y" -> 2 | "Z" -> 6 | _ -> 0)
-  | _ -> 0
+
+type choice = Rock | Paper | Scissors
+
+let int_of_choice c = match c with Rock -> 1 | Paper -> 2 | Scissors -> 3
+
+let match_result me opp =
+  match me with
+  | Rock -> ( match opp with Rock -> 3 | Paper -> 0 | Scissors -> 6)
+  | Paper -> ( match opp with Rock -> 6 | Paper -> 3 | Scissors -> 0)
+  | Scissors -> ( match opp with Rock -> 0 | Paper -> 6 | Scissors -> 3)
+
+let choice_of_string s =
+  match s with
+  | "A" | "X" -> Rock
+  | "B" | "Y" -> Paper
+  | "C" | "Z" -> Scissors
+  | _ -> raise (Invalid_argument (Format.sprintf "invalid choice %s" s))
 
 let rec sum_all l =
-    match l with
-    | [] -> 0
-    | h :: t -> (
-        sum_all t
-        +
-        match String.split_on_char ' ' h with
-        | first :: second :: _ -> calc_round first second
-        | _ -> 0)
+  match l with
+  | [] -> 0
+  | h :: t -> (
+      sum_all t
+      +
+      match String.split_on_char ' ' h with
+      | first :: second :: _ ->
+          let opp = choice_of_string first in
+          let me = choice_of_string second in
+          int_of_choice me + match_result me opp
+      | _ -> 0)
 
 let () =
-    let input_lines = read_whole_file "input.txt" in
-    print_endline (string_of_int (sum_all input_lines))
+  let input_lines = read_whole_file "input.txt" in
+  print_endline (string_of_int (sum_all input_lines))
 
 (* Part 2 *)
 (* Translate result to assumptions made in part 1 *)
-let subst_me opp res =
+
+type match_outcome = Win | Loss | Draw
+
+let match_outcome_of_string s =
+  match s with
+  | "X" -> Loss
+  | "Y" -> Draw
+  | "Z" -> Win
+  | _ -> raise (Invalid_argument (Format.sprintf "invalid outcome %s" s))
+
+let get_my_choice opp out =
   match opp with
-  | "A" -> ( match res with "X" -> "Z" | "Y" -> "X" | "Z" -> "Y" | _ -> "")
-  | "B" -> ( match res with "X" -> "X" | "Y" -> "Y" | "Z" -> "Z" | _ -> "")
-  | "C" -> ( match res with "X" -> "Y" | "Y" -> "Z" | "Z" -> "X" | _ -> "")
-  | _ -> ""
+  | Rock -> ( match out with Win -> Paper | Loss -> Scissors | Draw -> Rock)
+  | Paper -> ( match out with Win -> Scissors | Loss -> Rock | Draw -> Paper)
+  | Scissors -> (
+      match out with Win -> Rock | Loss -> Paper | Draw -> Scissors)
 
 let rec sum_all_2 l =
-    match l with
-    | [] -> 0
-    | h :: t -> (
-        sum_all_2 t
-        +
-        match String.split_on_char ' ' h with
-        | first :: second :: _ -> calc_round first (subst_me first second)
-        | _ -> 0)
+  match l with
+  | [] -> 0
+  | h :: t -> (
+      sum_all_2 t
+      +
+      match String.split_on_char ' ' h with
+      | first :: second :: _ ->
+          let opp = choice_of_string first in
+          let out = match_outcome_of_string second in
+          let me = get_my_choice opp out in
+          int_of_choice me + match_result me opp
+      | _ -> 0)
 
 let () =
-    let input_lines = read_whole_file "input.txt" in
-    print_endline (string_of_int (sum_all_2 input_lines))
+  let input_lines = read_whole_file "input.txt" in
+  print_endline (string_of_int (sum_all_2 input_lines))
