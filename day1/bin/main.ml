d612538cf0326b19ea980f898360d5bd36cad5a1
Petri-Johan Last <petri.last@gmail.com>
2023-08-21 20:14:28 +0200

    Switch to tail-recursive functions

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -2,36 +2,41 @@ let read_whole_file file =
   In_channel.open_text file |> In_channel.input_all |> String.split_on_char '\n'
 
 (* Part 1 *)
-let rec group l accum =
-  match l with
-  | [] -> accum
-  | h :: t -> (
-      match h with
-      | "" -> group t (0 :: accum)
-      | _ -> (
-          match accum with
-          | [] -> group t [ int_of_string h ]
-          | hd :: tl -> group t ((int_of_string h + hd) :: tl)))
+let group l =
+  let rec aux accum = function
+    | [] -> accum
+    | h :: t -> (
+        match h with
+        | "" -> aux (0 :: accum) t
+        | _ -> (
+            match accum with
+            | [] -> aux [ int_of_string h ] t
+            | hd :: tl -> aux ((int_of_string h + hd) :: tl) t))
+  in
+  aux [] l
 
-let rec max_of_list l cur =
-  match l with [] -> cur | h :: t -> max_of_list t (max h cur)
+let max_of_list l =
+  let rec aux cur = function [] -> cur | h :: t -> aux (max h cur) t in
+  aux 0 l
 
 let () =
   let input_list = read_whole_file "input.txt" in
-  print_endline (string_of_int (max_of_list (group input_list []) 0))
+  print_endline (string_of_int (max_of_list (group input_list)))
 
 (* Part 2 *)
-let rec max_3_of_list l (m1, m2, m3) =
-  match l with
-  | [] -> (m1, m2, m3)
-  | h :: t when h > m1 -> max_3_of_list t (h, m1, m2)
-  | h :: t when h > m2 -> max_3_of_list t (m1, h, m2)
-  | h :: t when h > m3 -> max_3_of_list t (m1, m2, h)
-  | _ :: t -> max_3_of_list t (m1, m2, m3)
+let max_3_of_list l =
+  let rec aux (m1, m2, m3) = function
+    | [] -> (m1, m2, m3)
+    | h :: t when h > m1 -> aux (h, m1, m2) t
+    | h :: t when h > m2 -> aux (m1, h, m2) t
+    | h :: t when h > m3 -> aux (m1, m2, h) t
+    | _ :: t -> aux (m1, m2, m3) t
+  in
+  aux (0, 0, 0) l
 
 let () =
   let input_list = read_whole_file "input.txt" in
   print_endline
     (string_of_int
-       (let m1, m2, m3 = max_3_of_list (group input_list []) (0, 0, 0) in
+       (let m1, m2, m3 = max_3_of_list (group input_list) in
         m1 + m2 + m3))
